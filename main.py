fc14ebd5f74c8e1ddac7abaa383e1a42421f2842
NeagaStefan <89096982+NeagaStefan@users.noreply.github.com>
2022-05-17 20:24:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup 
+import smtplib
+
+my_email = "stef.test99@gmail.com"
+password= "Tester19"
+
+
+URL="https://www.amazon.com/Logitech-Master-Advanced-Wireless-Mouse/dp/B07XC2FWD1"
+LANGUAGE= "en-US,en;q=0.9,ro;q=0.8"
+AGENT= "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
+DESIRED_PRICE= 100
+
+response= requests.get(URL, headers= {'Accept-Language' : LANGUAGE,
+            'User-Agent': AGENT})
+soup= BeautifulSoup(response.text,"html.parser")
+price= float(soup.find(name="span",class_="a-offscreen").getText().replace('$',""))
+print(price)
+
+if price<= DESIRED_PRICE:
+    message= f"Your product is under your desired price{DESIRED_PRICE} ! GO GET IT! {URL}"
+    with  smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password= password)
+
+            connection.sendmail(
+                from_addr =  my_email,
+                to_addrs= "recep.stef99@gmail.com",
+                msg= f"Subject:Cheap Product\n\n {message}")
+
